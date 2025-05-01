389cc96f2554c488a6dec1b9440231b0de2c3a5d
DCSS44 <dato_curiosso@outlook.com>
2025-05-01 14:28:51 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function actualizarTexto() {
-    const nuevoTextoPrincipal = prompt("Ingresa nuevo texto principal:", "Texto PARA TI Principal");
-    const nuevoTextoSecundario = prompt("Ingresa nuevo texto secundario:", "Texto Secundario");
-    
-    document.getElementById("texto-principal").innerText = nuevoTextoPrincipal;
-    document.getElementById("texto-secundario").innerText = nuevoTextoSecundario;
-}
